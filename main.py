5714d08e115ef1033f1adfb1ce6d760b65df0299
lathish.kumar <lathish2232@gmail.com>
2023-09-23 14:46:39 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def health_check():
     return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    #uvicorn.run(app, port=8080, host='127.0.0.1')
+    uvicorn.run(app, port=8080, host='0.0.0.0')
+
+# To run server use this bellow cmd (use any one )
+
+# -$> python -m uvicorn main:app --reload
+# -$> uvicorn main:app --reload
